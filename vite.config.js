f6f4832c146b136db008a24a6a8486db2e7e1fd6
imr4nn <218129649+syedimranhere@users.noreply.github.com>
2025-08-02 04:26:56 +0500

    vercel issue

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-//   server: {
-//     port: 6999,
-//     proxy: {
-//       "/api": {
-//         // target means backend server
-//         target: "http://localhost:7000",
-//       },
-//       //add more shortcuts/proxy prefixes if needed
-//     },
-//   },
-// });
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  //   server: {
+  //     port: 6999,
+  //     proxy: {
+  //       "/api": {
+  //         // target means backend server
+  //         target: "http://localhost:7000",
+  //       },
+  //       //add more shortcuts/proxy prefixes if needed
+  //     },
+  //   },
+});
 // the proxy only helps in development mode, so you can use relative paths in your axios requests
