ea0680cbaa65e61bf25dd156a0a3c96f60648be1
imr4nn <218129649+syedimranhere@users.noreply.github.com>
2025-08-03 03:43:54 +0500

    unauth issue fixed

--- src/pages/enterEmail.jsx
+++ src/pages/enterEmail.jsx
@@ -25,7 +25,7 @@ export default function ForgetPass1() {
         setLoading(true);
         setMessage("");
         try {
-            const response = await axios.post("/api/v1/user/send-otp", { email });
+            const response = await axios.post(`${import.meta.env.VITE_API_BASE_URL}/api/v1/user/send-otp`, { email });
             if (response?.data?.success) {
                 setMessage("OTP has been sent to your email.");
                 setOtpForm(true);
